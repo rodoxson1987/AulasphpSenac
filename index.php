d2c1e12e41bd39ca5382a3800c6d372488e5502c
Rodolfo Almeida <rodoxson1987@gmail.com>
2025-08-21 11:18:00 -0400

    Update index.php
    Add estruturas de repetição, arrays, criando tabela calendário.

--- index.php
+++ index.php
@@ -1,23 +1,38 @@
 <?php
-      function linha() {
-          return "
-              <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-               </tr>
-              
-               ";   
-      }
+
+function linha($semana) {
+    $linha = '<tr>';
+    for ($i = 0; $i <= 6; $i++) {
+        if (array_key_exists($i, $semana)){
+            $linha .= "<td> {$semana[$i]} </td>";
+        } else {
+            $linha .= '<td></td>';
+        }
+    }
+    $linha .= '</tr>';
+    return $linha;
+}
+
+function calendario(){
+    $calendario = '';
+    $dia = 1;
+    $semana = [];
+    while($dia <= 31){
+        array_push($semana, $dia);
+        if (count($semana) == 7){
+            $calendario .= linha($semana);
+            $semana = [];
+        }
+        $dia++;
+    }
+    // preenchendo os dias restantes da semana
+    #while (count($semana)< 7){
+        #array_push($semana, "");
+    #}
+    $calendario .= linha($semana);
+    return $calendario;
+}
 ?>      
-
-
-
-
 
 <table border="1">
     <tr>
@@ -29,5 +44,11 @@
         <th>Sex</th>
         <th>Sab</th>
 </tr>
- <?php echo linha(); ?>
-  
+
+<?php
+    echo calendario();
+
+    ?>
+
+
+
